19b0df2236123a80e51f39c38d7f794ff2545bae
Sean-Sophearom <sean.sophearom77@gmail.com>
2022-11-23 10:22:31 +0700

    add search

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,9 +6,12 @@ const Navbar = () => {
       <Link href="/" className="text-lg font-medium">
         Romflix
       </Link>
-      <ul className="flex gap-8">
+      <ul className="flex gap-2 text-xs xs:text-sm xs:gap-6 sm:text-base sm:gap-8">
         <li className="hover:underline hover:text-gray-500">
           <Link href="/movies">movies</Link>
+        </li>
+        <li className="hover:underline hover:text-gray-500">
+          <Link href="/search">search</Link>
         </li>
         <li className="hover:underline hover:text-gray-500">
           <Link href="/about">about</Link>
--- pages/movies/index.js
+++ pages/movies/index.js
@@ -1,42 +1,62 @@
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import MovieGrid from "../../components/MovieGrid";
 import Spinner from "../../components/Spinner";
 
-const Index = () => {
+const Index = ({ query }) => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [noMore, setNomore] = useState(false);
   const [page, setPage] = useState(1);
 
-  const getMovies = async (page) => {
+  const getMovies = async (page, query) => {
     setLoading(true);
-    const res = await fetch(
-      `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=0eb72e5a87c3896938cd899d9b93a334&page=${page}`
-    );
+    let res;
+    if (query) {
+      res = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=0eb72e5a87c3896938cd899d9b93a334&query=${query}&page=${page}`
+      );
+    } else {
+      res = await fetch(
+        `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=0eb72e5a87c3896938cd899d9b93a334&page=${page}`
+      );
+    }
     const data = await res.json();
     const results = data.results;
+    if (results.length === 0) {
+      setLoading(false);
+      return setNomore(true);
+    }
     setLoading(false);
-
     setMovies((prev) => [...prev, ...results]);
   };
 
   useEffect(() => {
-    getMovies(page);
-  }, [page]);
+    getMovies(page, query);
+  }, [page, query]);
 
   return (
     <>
-      <MovieGrid movies={movies} title="Popular Movies" />
+      <MovieGrid movies={movies} title={query ? `Search results for: ${query}` : "Popular Movies"} />
       <div className="p-4 flex items-center justify-center">
         {loading ? (
           <Spinner />
         ) : (
-          <button className="btn w-auto px-4 text-base" onClick={() => setPage(page + 1)}>
-            Load more
+          <button className="btn w-auto px-4 text-base" onClick={() => (noMore ? null : setPage(page + 1))}>
+            {noMore ? "No more results" : "Load more"}
           </button>
         )}
       </div>
     </>
   );
+};
+
+export const getServerSideProps = (ctx) => {
+  return {
+    props: {
+      query: ctx.query.q || null,
+    },
+  };
 };
 
 export default Index;
